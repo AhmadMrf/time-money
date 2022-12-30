409f922edc144608c701cccbb22e4270a0e74f7e
mrAhmad <naashna20@gmail.com>
2022-12-30 19:03:02 +0330

    add dailt tab functionality

--- src/daily-report/DailyTab.jsx
+++ src/daily-report/DailyTab.jsx
@@ -1,16 +1,22 @@
+import { mergeDayRecords } from "../utils/mergeDayRecords";
 import ContentWrapper from "../templates/ContentWrapper";
 import RowWrapper from "../templates/RowWrapper";
 import styles from "./DailyTab.module.css";
 import TotalDaily from "./TotalDaily";
 import DailyRow from "./DailyRow";
-export default function DailyTab({ children, className, ...rest }) {
+import { useGlobalContext } from "../context/record-context";
+const DailyTab = ({ children, className, ...rest }) => {
+  const { inMonthObject, loading, error } = useGlobalContext();
+  const daysRecords = mergeDayRecords(inMonthObject);
+  const activeDays = Object.keys(daysRecords);
   return (
     <ContentWrapper>
       <RowWrapper>
         <DailyRow title="منزل"></DailyRow>
         <DailyRow title="مطب"></DailyRow>
       </RowWrapper>
-      <TotalDaily />
+      <TotalDaily totalTime={4} totalPrice={200} activeDays={activeDays} />
     </ContentWrapper>
   );
-}
+};
+export default DailyTab;
